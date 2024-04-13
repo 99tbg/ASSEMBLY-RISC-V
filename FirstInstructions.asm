bdf535fa7c31c8952dcbcb3b0d9721c8400d942c
99tbg <166854743+99tbg@users.noreply.github.com>
2024-04-13 17:19:43 +0200

    done#X

--- FirstInstructions.asm
+++ FirstInstructions.asm
@@ -1,3 +1,4 @@
+#done
 #in the memory i put data, specifically two numbers.I want to write a code to load the first into
 #reg 3, the second in reg4. To dthis load word must be used. I need the address of the numbers
 #i want to register. For traditional resons we put our data starting from the address 010010000 so the second addresswill be 0x10010004
